e986ab579a29b3655119e91752ef291c693107eb
John Daniel Regino <time1015@users.noreply.github.com>
2020-04-18 21:27:19 +0800

    Introduce Injector abstract class
    Also, include a test implementation and an exception factory class.

---
+++ src/main/java/io/se7en/utils/dinjector/Injector.java
@@ -0,0 +1,25 @@
+package io.se7en.utils.dinjector;
+
+import java.util.Optional;
+
+public abstract class Injector {
+  public final <T> T inject(Class<T> type) {
+    if (type == null)
+      throw ExceptionsForInjector.nullType();
+
+    T toInject = onInject(type);
+    if (toInject == null)
+      throw ExceptionsForInjector.missing(type);
+
+    return toInject;
+  }
+
+  public final <T> Optional<T> injectOptional(Class<T> type) {
+    if (type == null)
+      throw ExceptionsForInjector.nullType();
+
+    return Optional.ofNullable(onInject(type));
+  }
+
+  protected abstract <T> T onInject(Class<T> type);
+}
---
+++ src/main/java/io/se7en/utils/dinjector/MissingDependencyException.java
@@ -0,0 +1,21 @@
+package io.se7en.utils.dinjector;
+
+public class MissingDependencyException extends RuntimeException {
+  private static final long serialVersionUID = -7817121600220800333L;
+
+  public MissingDependencyException() {
+    super();
+  }
+
+  public MissingDependencyException(String message) {
+    super(message);
+  }
+
+  public MissingDependencyException(Throwable cause) {
+    super(cause);
+  }
+
+  public MissingDependencyException(String message, Throwable cause) {
+    super(message, cause);
+  }
+}
